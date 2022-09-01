eb1f0438165de8c9bbb2e65ff1245cbc6253372f
Aneta-se <54153719+aneta-s@users.noreply.github.com>
2022-09-01 11:11:18 +0200

    adding service worker unregister

--- js/tilt.js
+++ js/tilt.js
@@ -303,3 +303,8 @@
 
     return true;
 }));
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister()
+      document.location.reload()
+  } })
